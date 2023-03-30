a50b4dda97156fb46516dc086c71828882bc20b5
tugalsan karabacak <tugalsan@gmail.com>
2023-03-30 12:41:47 +0300

    .

--- src/main/java/com/tugalsan/api/id/server/TS_IdMachineUtils.java
+++ src/main/java/com/tugalsan/api/id/server/TS_IdMachineUtils.java
@@ -1,14 +1,14 @@
 package com.tugalsan.api.id.server;
 
-import com.tugalsan.api.os.server.TS_OSUtils;
-import com.tugalsan.api.os.server.TS_Process;
+import com.tugalsan.api.os.server.TS_OsPlatformUtils;
+import com.tugalsan.api.os.server.TS_OsProcess;
 import com.tugalsan.api.string.server.TS_StringUtils;
 
 public class TS_IdMachineUtils {
 
     public static String get() {
-        if (TS_OSUtils.isWindows()) {
-            var str = TS_Process.of("wmic csproduct get UUID").output;
+        if (TS_OsPlatformUtils.isWindows()) {
+            var str = TS_OsProcess.of("wmic csproduct get UUID").output;
             var lst = TS_StringUtils.toList_spc(str);
             for (var item : lst) {
                 item = item.trim();
@@ -18,12 +18,12 @@ public class TS_IdMachineUtils {
                 return item;
             }
             return null;
-        } else if (TS_OSUtils.isMac()) {
-            return TS_Process.of("system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }").output;
-        } else if (TS_OSUtils.isLinux()) {
-            return TS_Process.of("# cat /sys/class/dmi/id/product_uuid").output;
-        } else if (TS_OSUtils.isUnix()) {
-            return TS_Process.of(new String[]{
+        } else if (TS_OsPlatformUtils.isMac()) {
+            return TS_OsProcess.of("system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }").output;
+        } else if (TS_OsPlatformUtils.isLinux()) {
+            return TS_OsProcess.of("# cat /sys/class/dmi/id/product_uuid").output;
+        } else if (TS_OsPlatformUtils.isUnix()) {
+            return TS_OsProcess.of(new String[]{
                 "/bin/sh",
                 "-c",
                 "echo <password for superuser> | sudo -S cat /sys/class/dmi/id/product_uuid"
