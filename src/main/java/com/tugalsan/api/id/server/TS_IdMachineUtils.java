f34bfafe1a40f06a5b7925206460cb49c1de7fc3
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-11 10:17:57 +0300

    .

--- src/main/java/com/tugalsan/api/id/server/TS_IdMachineUtils.java
+++ src/main/java/com/tugalsan/api/id/server/TS_IdMachineUtils.java
@@ -3,33 +3,49 @@ package com.tugalsan.api.id.server;
 import com.tugalsan.api.os.server.TS_OsPlatformUtils;
 import com.tugalsan.api.os.server.TS_OsProcess;
 import com.tugalsan.api.string.server.TS_StringUtils;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 
 public class TS_IdMachineUtils {
 
-    public static String get() {
+    public static TGS_UnionExcuse<String> get() {
         if (TS_OsPlatformUtils.isWindows()) {
-            var str = TS_OsProcess.of("wmic csproduct get UUID").output;
-            var lst = TS_StringUtils.toList_spc(str);
+            var u = TS_OsProcess.of("wmic csproduct get UUID").toUnion();
+            if (u.isExcuse()) {
+                return u.toExcuse();
+            }
+            var lst = TS_StringUtils.toList_spc(u.value().output);
             for (var item : lst) {
                 item = item.trim();
                 if (item.isEmpty() || item.equals("UUID")) {
                     continue;
                 }
-                return item;
+                return TGS_UnionExcuse.of(item);
             }
             return null;
         } else if (TS_OsPlatformUtils.isMac()) {
-            return TS_OsProcess.of("system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }").output;
+            var u = TS_OsProcess.of("system_profiler SPHardwareDataType | awk '/UUID/ { print $3; }").toUnion();
+            if (u.isExcuse()) {
+                return u.toExcuse();
+            }
+            return TGS_UnionExcuse.of(u.value().output);
         } else if (TS_OsPlatformUtils.isLinux()) {
-            return TS_OsProcess.of("# cat /sys/class/dmi/id/product_uuid").output;
+            var u = TS_OsProcess.of("# cat /sys/class/dmi/id/product_uuid").toUnion();
+            if (u.isExcuse()) {
+                return u.toExcuse();
+            }
+            return TGS_UnionExcuse.of(u.value().output);
         } else if (TS_OsPlatformUtils.isUnix()) {
-            return TS_OsProcess.of(new String[]{
+            var u = TS_OsProcess.of(new String[]{
                 "/bin/sh",
                 "-c",
                 "echo <password for superuser> | sudo -S cat /sys/class/dmi/id/product_uuid"
-            }).output;
+            }).toUnion();
+            if (u.isExcuse()) {
+                return u.toExcuse();
+            }
+            return TGS_UnionExcuse.of(u.value().output);
         } else {
-            return null;
+            return TGS_UnionExcuse.ofExcuse("TS_IdMachineUtils", "get", "unknown os");
         }
     }
 }
