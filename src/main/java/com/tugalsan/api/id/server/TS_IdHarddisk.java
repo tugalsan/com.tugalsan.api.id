359fed9407b331ee65f3a6f3ccb62e1c6b419a7f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-15 12:18:44 +0300

    .

--- src/main/java/com/tugalsan/api/id/server/TS_IdHarddisk.java
+++ src/main/java/com/tugalsan/api/id/server/TS_IdHarddisk.java
@@ -2,21 +2,21 @@ package com.tugalsan.api.id.server;
 
 import java.nio.file.*;
 import java.util.*;
-import com.tugalsan.api.stream.client.*;
-import com.tugalsan.api.unsafe.client.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
+import java.io.IOException;
 
 public class TS_IdHarddisk {
 
-    public static List<String> get() {
+    public static List<TGS_UnionExcuse<String>> get() {
         //TODO get: add LINUX implementaion: hdparm -i /dev/hda 
-        return TGS_StreamUtils.toLst(
-                TGS_StreamUtils.of(FileSystems.getDefault().getFileStores())
-                        .map(item -> TGS_UnSafe.call(() -> {
-                    return String.valueOf(item.getAttribute("volume:vsn"));
-                }, e -> {
-                    e.printStackTrace();
-                    return "N/A";
-                }))
-        );
+        List<TGS_UnionExcuse<String>> all = new ArrayList();
+        for (var fs : FileSystems.getDefault().getFileStores()) {
+            try {
+                all.add(TGS_UnionExcuse.of(String.valueOf(fs.getAttribute("volume:vsn"))));
+            } catch (IOException ex) {
+                all.add(TGS_UnionExcuse.ofExcuse(ex));
+            }
+        }
+        return all;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.tugalsan.api.id {
     requires com.tugalsan.api.os;
     requires com.tugalsan.api.string;
-    requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.stream;
