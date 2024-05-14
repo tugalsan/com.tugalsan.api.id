5c22551369133880e3cb947399535d73e31206bd
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-05-14 07:55:41 +0300

    .

--- src/main/java/com/tugalsan/api/id/server/TS_IdHarddisk.java
+++ src/main/java/com/tugalsan/api/id/server/TS_IdHarddisk.java
@@ -3,19 +3,19 @@ package com.tugalsan.api.id.server;
 import java.nio.file.*;
 import java.util.*;
 import com.tugalsan.api.stream.client.*;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import com.tugalsan.api.unsafe.client.*;
 
 public class TS_IdHarddisk {
 
-    public static List<String> get() {
+    public static List<TGS_UnionExcuse<String>> get() {
         //TODO get: add LINUX implementaion: hdparm -i /dev/hda 
         return TGS_StreamUtils.toLst(
                 TGS_StreamUtils.of(FileSystems.getDefault().getFileStores())
                         .map(item -> TGS_UnSafe.call(() -> {
-                    return String.valueOf(item.getAttribute("volume:vsn"));
+                    return TGS_UnionExcuse.of(String.valueOf(item.getAttribute("volume:vsn")));
                 }, e -> {
-                    e.printStackTrace();
-                    return "N/A";
+                    return TGS_UnionExcuse.ofExcuse(e);
                 }))
         );
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.tugalsan.api.id {
     requires com.tugalsan.api.os;
     requires com.tugalsan.api.string;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.stream;
