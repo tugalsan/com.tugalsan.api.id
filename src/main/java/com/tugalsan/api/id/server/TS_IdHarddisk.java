672d7bdce0989ae5224e104719a7fbcd7900932b
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-10 11:28:42 +0300

    .

--- src/main/java/com/tugalsan/api/id/server/TS_IdHarddisk.java
+++ src/main/java/com/tugalsan/api/id/server/TS_IdHarddisk.java
@@ -1,10 +1,11 @@
 package com.tugalsan.api.id.server;
 
+import com.tugalsan.api.function.client.maythrow.checkedexceptions.TGS_FuncMTCEUtils;
 import java.nio.file.*;
 import java.util.*;
 import com.tugalsan.api.stream.client.*;
 import com.tugalsan.api.union.client.TGS_UnionExcuse;
-import com.tugalsan.api.unsafe.client.*;
+
 
 public class TS_IdHarddisk {
 
@@ -12,7 +13,7 @@ public class TS_IdHarddisk {
         //TODO get: add LINUX implementaion: hdparm -i /dev/hda 
         return TGS_StreamUtils.toLst(
                 TGS_StreamUtils.of(FileSystems.getDefault().getFileStores())
-                        .map(item -> TGS_UnSafe.call(() -> {
+                        .map(item -> TGS_FuncMTCEUtils.call(() -> {
                     return TGS_UnionExcuse.of(String.valueOf(item.getAttribute("volume:vsn")));
                 }, e -> {
                     return TGS_UnionExcuse.ofExcuse(e);
